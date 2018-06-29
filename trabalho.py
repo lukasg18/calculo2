50e327a698b979031163f50394b3708de300c789
lukasg18 <lukas.gomes2010@gmail.com>
2018-06-29 17:19:04 -0300

    passo 4 em progresso

--- trabalho.py
+++ trabalho.py
@@ -1,47 +1,48 @@
-from functools import wraps
+from scipy.integrate import quad
+import numpy as np
+import csv
 
-def memo(f):
-  # Decorador de "memória" para programação dinâmica.
-  @wraps(f)
-  def func(*args):
-    if args not in func.cache:
-      func.cache[args] = f(*args)
-    return func.cache[args]
-  func.cache = {}
-  return func
-
-
-# Função fatorial
-@memo
-def fatorial(x):
-  if x == 0:
-    return 1
-  return x * fatorial(x-1)
-
-
-# funcao de distribuicao normal
-def distribuicaoNormal(x):
-    i = 0
-    # while(i < )
-    return ((1/((2*3.14)**(1/2))) * serieMclariun(x)) #  (1/(2.pi)) ^ -x²/2 
-
-# calculando exp com serie de Mclariun
-def serieMclariun(x):
+def f(x):
+    vet = []
     i = 0
     soma = 0
     while(i < 30):
         fat = fatorial(i)
-        soma =  soma + ((((-x**2)/2)**i)/fat) # (((-x²)/2)^n)/n!
-        # print(soma) 
+        soma = soma + ((((-x**2)/2)**i)/fat) # concatena uma unica expressao dentro da integral
         i +=1
-    return soma
+    return soma # expressao a ser integrada pela biblioteca "scipy.integrate"
 
+def fatorial(x):
+    if(x <= 1):
+        return 1
+    return fatorial(x-1) * x
+
+def geraTabela():
+    c = csv.writer(open("teste.csv", "wb"))
+    x = 0
+    j = 0
+    i = 0.0
+    vet = []
+    matriz = []
+    while (i < 4): # a variavel i varia de 0.0 ate 3.9 (coluna da tabela, veja o exemplo q a professora colocou no drive)
+        while (j < 10): # linha da tabela varia de 1 a 9
+            b = i + (j/100.0) # incrementando de 0.01 por 0.01
+            resultIntegral = quad(f,0,b) # funcao q calcula a integral onde os parametros sao: (expressao a ser integrada, intervalo de integracao,intervalo de integracao)
+            aux = (1/((2*3.14159265359)**(0.5))) # constante da formula de ditribuicao normal padrao(exmplo descrito no trabalho). 
+            valor = resultIntegral[0] * aux # multiplicando o resultado da integral pela constante. o resultado do integral se encontra na primeira posicao do vetor, por isso eu multiplico pela posicao 0.
+            vet.append(valor) #salvando o resultado em um vetor(linha a linha da tabela "0.1", "0.2", "0.3", ...)
+            j += 1
+        c.writerow(vet)
+        matriz.append(vet) # salvando a linha da tabela em uma matriz.
+        vet = []
+        i += 0.1
+        j = 0
+    np.set_printoptions(precision=6, linewidth=200) #configurando o modo de visualizacao da matriz, onde o "precision" e a quantidade de casas decimais apos a virgula e o "linewidth" e quantidade de termos a serem imprimidas em uma linha
+    print(np.matrix(matriz)) # usando numpy para imprimir a matriz
 
 def main(args):
-    # x = fatorial(4)
-    x = distribuicaoNormal(0.00)
-    # x = serieMclariun(0.00)
-    print(x)
+
+    geraTabela()
     return 0
 
 if __name__ == '__main__':
